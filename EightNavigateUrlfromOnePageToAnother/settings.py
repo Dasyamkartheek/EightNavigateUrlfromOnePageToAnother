a3f6cb0a9b5a27443c23717bd7c1e512959a37da
DasyamKartheek <satyadasyam8@gmail.com>
2024-12-22 19:27:07 +0530

    nav4

--- EightNavigateUrlfromOnePageToAnother/settings.py
+++ EightNavigateUrlfromOnePageToAnother/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-63+zej0w@)u6u0tn=ml+3eydtff(i=yzq4a7#8($=w-!s%!%sf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
